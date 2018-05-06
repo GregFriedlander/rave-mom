62f5ff7690df66198551c31055bbc251b67b6f27
Greg Friedlander <gregfriedlander@gmail.com>
2018-05-06 06:56:46 -0700

    add service

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,59 +1,26 @@
 import { Component, OnInit, Input } from '@angular/core';
-// import { GeoJSONSourceComponent } from 'ngx-mapbox-gl/app/lib/source/geojson/geojson-source.component';
-// import { GeoJSONSourceComponent } from '../../../node_modules/ngx-mapbox-gl/app/lib/source/geojson/geojson-source.component';
+import { ApiHttpService } from '../api-http.service';
+
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
-//   template: 
-//   `
-//   <mgl-map
-//     [style]="'mapbox://styles/mapbox/dark-v9'"
-//     [zoom]="[16]"
-//     [center]="[-115.010276, 36.272285]"
-// >
-//           <mgl-geojson-source
-//           id="symbols-source"
-//           >
-//               <mgl-feature
-//                 *ngFor="let geometry of geometries"
-//                 [geometry]="geometry"
-//               >
-//               </mgl-feature>
-//           </mgl-geojson-source>
-//   </mgl-map>
-//   `,
-//   styles: [`
-//     mgl-map {
-//       height: 500px;
-//       width: 100%;
-//     }
-//     element {
-
-//       height: 2000px;
-  
-//   }
-
-//   `]
 })
 export class MapComponent implements OnInit {
 
-  // @Input() geo: GeoJSONSourceComponent;
 
-  constructor() { 
-
-  //   var map = {
-  //     container: 'map',
-  //     style: 'mapbox://styles/mapbox/dark-v9',
-  //     center: [-115.010276, 36.272285],
-  //     zoom: 16
-  // };
-
-  }
+  constructor(public service: ApiHttpService) { }
 
   ngOnInit() {
-    
+    this.getUserData();
+  }
+
+  getUserData(){
+    let observable = this.service.getInfo();
+    observable.subscribe((data:any)=>{
+      console.log(data);
+    })
   }
   
 
